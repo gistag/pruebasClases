8537a49b070e86f57313d36248ab77b61c1bc362
Gissel Gutierrez <gutierrez.gissel@gmail.com>
2022-09-01 09:49:06 -0500

    Creacion de repositorios y servicios de empresa

--- src/main/java/com/udeA/Ciclo3/Ciclo3Application.java
+++ src/main/java/com/udeA/Ciclo3/Ciclo3Application.java
@@ -1,13 +1,34 @@
 package com.udeA.Ciclo3;
 
+import com.udeA.Ciclo3.modelos.Empresa;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+@RestController
+@SpringBootApplication (exclude ={SecurityAutoConfiguration.class})
 public class Ciclo3Application {
+
+	@GetMapping("/hello")
+	public String hello(){
+		return "ciclo 3.. saldremos vivos de este!";
+	}
+
+	@GetMapping("/test")
+	public String test(){
+
+		Empresa emp = new Empresa("SOLAR SAS", "Calle la geta","3213213211", "800212121");
+		emp.setNombre("SOLAR LTDA");
+		return emp.getNombre();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Ciclo3Application.class, args);
+		//System.out.println("Hola Mundo");
+
 	}
+
 
 }
---
+++ src/main/java/com/udeA/Ciclo3/service/EmpresaService.java
@@ -0,0 +1,39 @@
+package com.udeA.Ciclo3.service;
+
+import com.udeA.Ciclo3.modelos.Empresa;
+import com.udeA.Ciclo3.repo.EmpresaRepositorio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class EmpresaService {
+    @Autowired //Conectamos esta clase con el repository de Empresa
+    EmpresaRepositorio empresaRepository; //Creamos un objeto tipo EmpresaRepository para poder usar los metodos que dicha clase hereda
+
+    //Método que retorna la lista de empresas usando heredados del jpaRepository
+    public List<Empresa>getAllEmpresas(){
+        List<Empresa> empresalist = new ArrayList<>();
+        empresaRepository.findAll().forEach(empresa->empresalist.add(empresa));//Recorremos el iterable que regresa el método findAll del JPA y lo guardamos en la lista
+        return empresalist;
+    }
+
+    //Metodo que se trae un objeto de tipo Emmpresa usando el id de la misma
+    public Empresa getEmpresaById(Integer id){
+        return empresaRepository.findById(id).get();
+    }
+
+
+    //Metodo para guardar o actualizar objetos de tipo Empresa
+    public boolean saveOrUpdateEmpresa(Empresa empresa){
+        Empresa emp = empresaRepository.save(empresa);
+        if(empresaRepository.findById(emp.getId())!=null){
+            return true;
+        }
+        return false;
+    }
+//método delete pendiente
+}
+
